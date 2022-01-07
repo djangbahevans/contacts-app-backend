2ef03bd92e64323b8dc8a5d8ac16cd30c8dafaa7
Evans Djangbah <djangbahevans@yahoo.com>
2022-01-07 23:27:55 +0000

    Added cors wildcard

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware,
-                   allow_origins=[],
+                   allow_origins=['*'],
                    allow_credentials=True,
                    allow_methods=['*'],
                    allow_headers=['*'])
