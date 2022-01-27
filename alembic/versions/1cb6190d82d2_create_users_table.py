76a20f76b8868bb3c291ad14163b0969a6c63e3f
Evans Djangbah <djangbahevans@yahoo.com>
2022-01-27 08:55:29 +0000

    Created users table db migration

---
+++ alembic/versions/1cb6190d82d2_create_users_table.py
@@ -0,0 +1,28 @@
+"""Create users table
+
+Revision ID: 1cb6190d82d2
+Revises: 
+Create Date: 2022-01-27 08:32:26.561690
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '1cb6190d82d2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('users',
+                    sa.Column("id", sa.Integer,
+                              primary_key=True, nullable=False),
+                    sa.Column("email", sa.String, nullable=False, unique=True),
+                    sa.Column("password", sa.String, nullable=False),
+                    sa.Column("created_at", sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.sql.expression.text("now()")))
+
+
+def downgrade():
+    op.drop_table('users')
--- app/models.py
+++ app/models.py
@@ -40,5 +40,4 @@ class User(Base):
     id = Column("id", Integer, primary_key=True, nullable=False)
     email = Column("email", String, nullable=False, unique=True)
     password = Column("password", String, nullable=False)
-    created_at = Column("created_at", TIMESTAMP(
-        timezone=True), nullable=False, server_default=text("now()"))
+    created_at = Column("created_at", TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
